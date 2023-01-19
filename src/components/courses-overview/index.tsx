198ff6bfafc8143faa743c52a5cdc470f26ad91d
Ruben Bottu <ruben.bottu@student.ucll.be>
2023-01-19 17:19:52 +0000

    Added status messages and delayed "calculate" appearance

--- src/components/courses-overview/index.tsx
+++ src/components/courses-overview/index.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames';
 import React, { useState, useEffect } from 'react';
-import { Course } from '../../types';
+import { Course, StatusMessage } from '../../types';
 import CourseService from '../../services/CourseService';
 import { AxiosResponse } from 'axios';
 import CoursesOverviewTable from './CoursesOverviewTable';
@@ -8,9 +8,11 @@ import CoursesOverviewList from './CoursesOverviewList';
 
 // https://blog.logrocket.com/why-react-doesnt-update-state-immediately/
 const CourseOverview: React.FC = () => {
+    const MIN_AMOUNT_SELECTED_COURSES = 2;
     const [courses, setCourses] = useState<Course[]>([]);
     const [courseNameSearchInput, setCourseNameSearchInput] = useState<string>('');
     const [selectedCourses, setSelectedCourses] = useState<Course[]>([]);
+    const [statusMessages, setStatusMessages] = useState<StatusMessage[]>([]);
 
     useEffect(() => {
         getCourses();
@@ -28,6 +30,8 @@ const CourseOverview: React.FC = () => {
     };
 
     const onSelectCourse = (course: Course) => {
+        // if the course is already selected, tell the user it can only be selected once
+        // setStatusMessages([{ message: 'Course is already selected', type: 'error' }]);
         setCourseNameSearchInput('');
         setSelectedCourses([...selectedCourses, course]);
     };
@@ -40,6 +44,21 @@ const CourseOverview: React.FC = () => {
             <p className="mb-md-4">
                 Select the courses that you want to follow and generate your ideal class schedule
             </p>
+            {statusMessages && (
+                <ul className="list-unstyled col-4 mb-3">
+                    {statusMessages.map(({ message, type }, index) => (
+                        <li
+                            key={index}
+                            className={classNames({
+                                'text-danger': type === 'error',
+                                'text-success': type === 'success',
+                            })}
+                        >
+                            {message}
+                        </li>
+                    ))}
+                </ul>
+            )}
             <section className="mb-md-5">
                 <input
                     type="text"
@@ -51,7 +70,7 @@ const CourseOverview: React.FC = () => {
                 <CoursesOverviewList courses={foundCourses()} handleOnClick={onSelectCourse} />
             </section>
             <CoursesOverviewTable courses={selectedCourses} />
-            {selectedCourses && !!selectedCourses.length && (
+            {selectedCourses && selectedCourses.length >= MIN_AMOUNT_SELECTED_COURSES && (
                 <button onClick={onCalculateProposals} className="btn btn-primary mt-md-2">
                     Calculate
                 </button>
